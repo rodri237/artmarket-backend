7c277c9f621e969ace6c99167a079a302ab1f4cc
rodri237 <rodripone420@gmail.com>
2026-04-20 13:10:53 +0100

    Create ArtMarketApplication.java as the main Spring Boot application class

---
+++ src/main/java/com/artmarket/ArtMarketApplication.java
@@ -0,0 +1,12 @@
+package com.artmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtMarketApplication.class, args);
+    }
+}
